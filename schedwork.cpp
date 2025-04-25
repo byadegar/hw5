3de16921e93493037c50356dad4c69751006f256
Bella Yadegar <byadegar@usc.edu>
2025-04-25 00:33:14 +0000

    commit for schedwork and wordle

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool backtrackingFunction(int day, int workerIndex, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& countShifts); 
+//current day scheduling, current index worker on the day, availability matrix, number of workers needed for that day
+//max shifts allowed for each worker, output schedulel being built, number of shifts each worker has worked 
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +39,66 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    //do i have to do another check for empty??
+    
+    //get info for number of days and workers, grab the data from the 2d matrix
+    //rows represent days and columns represent workers and if true theyre free that day 
+    size_t numberDays = avail.size();  //how many days are there 
+    size_t numberWorkers = avail[0].size(); //how many workers are in there (cols in row)
 
+    //initialize schedule matrix, intiially with invalid_id so all slots are unassigned for now
+    sched.assign(numberDays, vector<Worker_T>(dailyNeed, INVALID_ID)); 
 
+    //initilize countshift vector to track shifts worked per each worker
+    vector<int> countShifts(numberWorkers, 0); 
 
+    //use all info to recursively schedule (using backtracking)
+    return backtrackingFunction(0, 0, avail, dailyNeed, maxShifts, sched, countShifts); 
 
 }
+
+bool backtrackingFunction(int day, int workerIndex, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& countShifts){
+  size_t numberDays = avail.size();  //how many days are there 
+  size_t numberWorkers = avail[0].size(); //how many workers are in there (cols in row)
+
+  //base case 
+  //all days have been scjeduled, your done 
+  if (day == int(numberDays)){ //means you have just finished scheduling the last day
+    return true; 
+  }
+
+  if (workerIndex == int(dailyNeed)){ //all slots are filled for this day, so we are looking at next day
+    return backtrackingFunction(day + 1, 0, avail, dailyNeed, maxShifts, sched, countShifts); 
+  }
+
+  //try assinging each worker to the current day and worker slot 
+  //see if workers are a valid choice 
+  for (Worker_T i = 0; i < numberWorkers; i++){
+    //every worker loop so we can try to place 
+    //check if worker is available on the day 
+    if (avail[day][i] == true){
+      if (countShifts[i] < int(maxShifts)){ //check if worker has exceeded the shift limkit
+        if (find(sched[day].begin(), sched[day].end(), i) == sched[day].end()) { //is worker already scheduled for this day
+          //yay able to go and assign the worker 
+          sched[day][workerIndex] = i; 
+          countShifts[i]++; 
+
+          //time to fill the next slot
+          //recursion 
+          if(backtrackingFunction(day, workerIndex +1, avail, dailyNeed, maxShifts, sched, countShifts) == true){
+            return true; //worked
+          }
+
+          //if u didn't return true, you have to backtrack to undo the assingment
+          sched[day][workerIndex] = INVALID_ID; 
+          countShifts[i]--; 
+        }
+
+      }
+    }
+  }
+  //if havent returned true by now 
+  return false; 
+} 
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void helpBuildWords(const string& in, string curr, int i, string floating, const set<string>& dict, set<string>& endResults); 
+//pattern string, word being built, cur position, remaning floating letters, dict of words, final set that works
 
 
 // Definition of primary wordle function
@@ -22,7 +24,62 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> endResults; 
+    helpBuildWords(in, "", 0, floating, dict, endResults); 
+    return endResults; 
 
 }
 
 // Define any helper functions here
+void helpBuildWords(const string& in, string curr, int i, string floating, const set<string>& dict, set<string>& endResults){
+    //base case 
+    //finished building the word 
+    if (i == in.size()){
+        if (floating.empty() && dict.find(curr)!= dict.end()){//if all floating letters have been used, and its in dict
+            endResults.insert(curr); //add it to the end results of words
+        }
+        return; // your out bc you did it 
+    }
+    //check if position is fixed or not when your building 
+    if (in[i] != '-'){
+      helpBuildWords(in, curr + in[i], i+1, floating, dict, endResults); //recursive call bc you dont wanna tocuh that
+    }
+    else {
+      //start going through the letters in the alphabet 
+      for (char character = 'a'; character <= 'z'; character++){
+        string next = curr + character; //build next version
+        string copyFloating = floating; //copy to save so we can modify
+
+        //chekc if character isinside of the floating string 
+        bool foundInString = false; 
+        int indextoRemove = 0; 
+        for (int i = 0; i < copyFloating.size(); i++){
+          if (copyFloating[i] == character){
+            foundInString = true; 
+            indextoRemove = i; 
+            break; 
+          }
+        }
+        if (foundInString == true){
+          //remove the used floarting letter 
+          copyFloating.erase(indextoRemove, 1); 
+          helpBuildWords(in, next, i+1, copyFloating, dict, endResults); 
+        }
+        else {
+          //letter is not floating 
+          //check blanks 
+          int dashesRemaining = 0; 
+          for (int j = i+1; j <in.size(); j++){ //go through word to count blanks
+            if (in[j] == '-'){
+              dashesRemaining++; 
+            }
+          }
+          if ((int)floating.size() <= dashesRemaining){
+            //you have enough room to place more letters so you call again
+            helpBuildWords(in, next, i+1, floating, dict, endResults); 
+          }
+        }
+      }
+    }
+}
+
